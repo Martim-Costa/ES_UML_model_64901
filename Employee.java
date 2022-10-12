7b01583f45f1cba60b5c8c49060a6587329a6751
Martim-Costa <mfe.costa@campus.fct.unl.pt>
2022-10-12 17:38:12 +0100

    Initial commit

---
+++ Employee.java
@@ -0,0 +1,28 @@
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class Employee extends University Member {
+
+    /**
+     * Default constructor
+     */
+    public Employee() {
+    }
+
+    /**
+     * 
+     */
+    private int acctNo;
+
+    /**
+     * @return
+     */
+    public int getAcctNo() {
+        // TODO implement here
+        return 0;
+    }
+
+}
---
+++ University Member.java
@@ -0,0 +1,30 @@
+
+import java.util.*;
+
+/**
+ * 
+ */
+public abstract class University Member {
+
+    /**
+     * Default constructor
+     */
+    public University Member() {
+    }
+
+    /**
+     * 
+     */
+    public String firstName;
+
+    /**
+     * 
+     */
+    public String LastName;
+
+    /**
+     * 
+     */
+    public int ssNo;
+
+}
